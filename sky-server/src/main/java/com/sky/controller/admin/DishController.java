311a2ec9a75f84037a1e48af99424368b0279582
itcast <hello@itcast.cn>
2026-08-22 12:06:01 +0800

    菜品起售、停售代码开发

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -54,4 +54,11 @@ public class DishController {
         dishService.updateWithFlavor(dishDTO);
         return Result.success();
     }
+    @ApiOperation("停售/起售菜品")
+    @PostMapping("/status/{status}")
+    public Result startOrStop(@PathVariable Integer status,  Long id){
+        log.info("停售/起售菜品：{}",id);
+        dishService.startOrStop(status,id);
+        return Result.success();
+    }
 }
--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -105,4 +105,12 @@ public class DishServiceImpl implements DishService {
         dishFlavorMapper.insertBatch(flavors);
         }
     }
+
+    @Override
+    public void startOrStop(Integer status, Long id) {
+        Dish dish=new Dish();
+        dish.setId(id);
+        dish.setStatus(status);
+        dishMapper.update(dish);
+    }
 }
