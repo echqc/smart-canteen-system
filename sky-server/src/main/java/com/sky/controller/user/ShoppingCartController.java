916b818d0f1b62fa90b2bd64e03ca3460e70be49
itcast <hello@itcast.cn>
2026-08-26 13:58:38 +0800

    查看购物车业务代码开发

--- sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -1,14 +1,16 @@
 package com.sky.controller.user;
 import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+import com.sky.mapper.ShoppingCartMapper;
 import com.sky.result.Result;
 import com.sky.service.ShoppingCartService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user/shoppingCart")
@@ -17,12 +19,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class ShoppingCartController {
     @Autowired
     private ShoppingCartService shoppingCartService;
+    @Autowired
+    private ShoppingCartMapper shoppingCartMapper;
+
     @PostMapping("/add")
+    @ApiOperation("添加购物车")
     public Result add(@RequestBody ShoppingCartDTO shoppingCartDTO){
         log.info("购物车数据：{}",shoppingCartDTO);
         shoppingCartService.add(shoppingCartDTO);
 
         return Result.success();
+
+    }
+    @ApiOperation("查看购物车")
+    @GetMapping("/list")
+    public Result<List<ShoppingCart>> list(){
+        List<ShoppingCart> list = shoppingCartService.showShoppingCart();
+        return Result.success(list);
 
     }
 }
