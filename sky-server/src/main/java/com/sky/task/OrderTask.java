6f5c3adc2ae0294bd1df4182bef6c4bbd7804361
itcast <hello@itcast.cn>
2026-08-31 21:48:53 +0800

    来单提醒业务代码开发

--- sky-server/src/main/java/com/sky/task/OrderTask.java
+++ sky-server/src/main/java/com/sky/task/OrderTask.java
@@ -3,6 +3,7 @@ package com.sky.task;
 import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @Slf4j
 @Component
 public class OrderTask {
+    @Autowired
     private OrderMapper orderMapper;
     @Scheduled(cron="0 * * * * ?")
     public void processTimeOutOrder() {
